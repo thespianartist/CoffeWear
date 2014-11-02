068b37722e76cc7cb7002e05fafe81050f41b2b6
Uriel Rodolfo Ramirez Moreno <topzihxc@gmail.com>
2014-11-02 10:01:06 -0600

    Agregando aplicacion movil para enviar la voz, ya que Android Wear por si solo no puede (:

--- coffewearwatch/src/main/java/la/mejorando/helloandroidwear/MainActivity.java
+++ coffewearwatch/src/main/java/la/mejorando/helloandroidwear/MainActivity.java
@@ -3,6 +3,8 @@ package la.mejorando.helloandroidwear;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
@@ -15,18 +17,34 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.wearable.DataMap;
+import com.mariux.teleport.lib.TeleportClient;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Activity{
 
 
     private static final int SPEECH_REQUEST_CODE = 0;
-    ImageView bean;
+    public ImageView bean;
+    TeleportClient mTeleportClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTeleportClient = new TeleportClient(getApplicationContext());
+
 
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
@@ -36,7 +54,11 @@ public class MainActivity extends Activity{
                 bean.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        displaySpeechRecognizer();
+                        Log.e("aiqjgygd8u97t87yuguhivbjhknbm sas","jsakljsa");
+                        //displaySpeechRecognizer();
+                        //sendData();
+                        mTeleportClient.setOnSyncDataItemTask(new ShowToastHelloWorldTask());
+
                     }
                 });
 
@@ -59,12 +81,37 @@ public class MainActivity extends Activity{
 
             List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
             String spokenText = results.get(0);
-
             Log.e("hola", spokenText);
             Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
+
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mTeleportClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mTeleportClient.disconnect();
+    }
+
+    public class ShowToastHelloWorldTask extends TeleportClient.OnSyncDataItemTask {
+
+        @Override
+        protected void onPostExecute(DataMap dataMap) {
+
+            String hello = dataMap.getString("hello");
+            Log.e("asas","jsakljsa");
+            Toast.makeText(getApplicationContext(),hello,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 
 
 
